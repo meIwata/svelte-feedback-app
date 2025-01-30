aee6ad4864ebfc9f2a9ce59884fef5b856360469
waleilulu <waleilulu@gmail.com>
2025-01-30 17:29:23 +0800

    Add new files

--- src/components/FeedbackForm.svelte
+++ src/components/FeedbackForm.svelte
@@ -1,11 +1,16 @@
 <script>
+    import {v4 as uuidv4} from "uuid"
     import Card from "./Card.svelte";
     import Button from "./Button.svelte";
+    import RatingSelect from "./RatingSelect.svelte";
     
     let text = ""
+    let rating = 10
     let btnDisabled = false
     let min = 10
     let message
+
+    const handleSelect = e => rating = e.detail
 
     const handleInput = () => {
         if(text.trim().length <= min){
@@ -16,13 +21,25 @@
             btnDisabled = false
         }
     }
+
+    const handleSubmit = () => {
+      if(text.trim().length > min){
+        const newFeedback = {
+          id: uuidv4(),
+          text,
+          rating: +rating
+        }
+        
+        console.log(newFeedback)
+      }
+    }
 </script>
 <Card>
     <header>
         <h2>How would you rate your service with us?</h2>
     </header>
-    <form>
-        <!-- Rating Select -->
+    <form on:submit|preventDefault={handleSubmit}>
+        <RatingSelect on:rating-select = {handleSelect}/>
         <div class="input-group">
             <input type="text" on:input={handleInput} bind:value={text} placeholder="Tell us something that keeps you coming back">
         <Button disabled={btnDisabled} type="submit">Send</Button> 
