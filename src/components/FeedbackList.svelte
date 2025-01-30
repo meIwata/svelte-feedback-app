a8a415e373be018b42a8b721a338667e0d9bfa91
waleilulu <waleilulu@gmail.com>
2025-01-30 16:01:21 +0800

    Add new files

--- src/components/FeedbackList.svelte
+++ src/components/FeedbackList.svelte
@@ -1,9 +1,12 @@
 <script>
+  import FeedbackItem from "./FeedbackItem.svelte";
+
     export let feedback = []
 
     // console.log(feedback)
 </script>
 
 {#each feedback as fb (fb.id)}
-    <h3>{fb.text}</h3>    
+    <!-- <h3>{fb.text}</h3>     -->
+    <FeedbackItem item={fb} />
 {/each}
